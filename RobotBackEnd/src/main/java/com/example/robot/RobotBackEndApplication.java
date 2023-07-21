979a21c0be93914a82f2748395f7a638a5a0edb2
aslafshaikh313 <aslafshaikh313@gmail.com>
2023-07-21 23:36:06 +0530

    FC

---
+++ RobotBackEnd/src/main/java/com/example/robot/RobotBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobotBackEndApplication.class, args);
+	}
+
+}
---
+++ RobotBackEnd/src/main/java/com/example/robot/model/Robot.java
@@ -0,0 +1,18 @@
+package com.example.robot.model;
+
+public class Robot {
+
+	private int batteryLevel;
+
+	public Robot() {
+		this.batteryLevel = 100;
+	}
+
+	public int getBatteryLevel() {
+		return batteryLevel;
+	}
+
+	public void setBatteryLevel(int batteryLevel) {
+		this.batteryLevel = batteryLevel;
+	}
+}
